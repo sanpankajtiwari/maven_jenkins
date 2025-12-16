7b9e7f63a588047813fd00569b8e27420a807693
sanpankajtiwari <pankaj.tiwari@sansoftwares.com>
2025-12-16 14:17:36 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This is a single-line comment.
+        /*
+         * This is a multi-line comment.
+         * The code below prints "Hello, World!" to the console.
+         */
+        System.out.println("Hello, World!");
+    }
+}
